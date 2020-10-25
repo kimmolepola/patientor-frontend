4bb801a24a31c509f6abcd51d53373d7179bf5a4
Kimmo Lepola <kimmo.lepola@gmail.com>
2020-10-25 22:02:36 +0200

    heroku

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 //export const apiBaseUrl = 'http://localhost:3001/api';
-const { BASE_URL } = process.env;
-export const apiBaseUrl = `${BASE_URL}/api`;
+const BASE_URL = `${process.env.REACT_APP_BACKEND}/api`;
+export const apiBaseUrl = BASE_URL;
